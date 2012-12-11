7e6d3d3e91846e447783d8ce67c4aff282baae0b
Manuel Guenther <manuel.guenther@idiap.ch>
2012-12-11 13:32:03 +0100

    Changed version to 1.0.0 to upload package to PyPI.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='xbob.db.arface',
-    version='1.0.0a1',
+    version='1.0.0',
     description='AR Face  Database Access API for Bob',
     url='http://github.com/bioidiap/xbob.db.arface',
     license='GPLv3',
