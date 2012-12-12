23ab311686008990e64bbc19b13408498d45bff9
Manuel Guenther <manuel.guenther@idiap.ch>
2012-12-12 13:53:25 +0100

    Added verbose output for the database creation.

--- xbob/db/arface/create.py
+++ xbob/db/arface/create.py
@@ -24,7 +24,7 @@ import os
 
 from .models import *
 
-def add_clients(session):
+def add_clients(session, verbose):
   """Adds the clients and split up the groups 'world', 'dev', and 'eval'"""
   # these are the ids that contain less than 26 images; 20 out of 136 identities
   # and some other ids, to round up 50 world identities
@@ -41,56 +41,70 @@ def add_clients(session):
   eval_ids_m = set(range(1,77)) - world_ids_m - dev_ids_m
   eval_ids_w = set(range(1,61)) - world_ids_w - dev_ids_w
 
-
+  if verbose: print 'Adding clients ...'
   # now, we have: world: 28 male, 22 female; dev and eval: 24 male and 19 female
   # add these clients
   for id in world_ids_m:
+    if verbose>1: print "  Adding client 'm-%03d' to the world set" % id
     session.add(Client("m-%03d"%id, 'world'))
   for id in world_ids_w:
+    if verbose>1: print "  Adding client 'w-%03d' to the world set" % id
     session.add(Client("w-%03d"%id, 'world'))
 
   for id in dev_ids_m:
+    if verbose>1: print "  Adding client 'm-%03d' to the dev set" % id
     session.add(Client("m-%03d"%id, 'dev'))
   for id in dev_ids_w:
+    if verbose>1: print "  Adding client 'w-%03d' to the dev set" % id
     session.add(Client("w-%03d"%id, 'dev'))
 
   for id in eval_ids_m:
+    if verbose>1: print "  Adding client 'm-%03d' to the eval set" % id
     session.add(Client("m-%03d"%id, 'eval'))
   for id in eval_ids_w:
+    if verbose>1: print "  Adding client 'w-%03d' to the eval set" % id
     session.add(Client("w-%03d"%id, 'eval'))
 
-def add_files(session, directory, extension):
+def add_files(session, directory, extension, verbose):
   """Adds the files from the given directory"""
   files = os.listdir(directory)
+  if verbose: print "Adding files ..."
   for file in files:
     parts = os.path.splitext(file)
     if parts[1] == extension:
+      if verbose > 1: print "  Adding file '%s'" % parts[0]
       session.add(File(parts[0]))
 
-def add_protocols(session):
+def add_protocols(session, verbose):
   """Adds various protocols for the AR face database"""
+  if verbose: print "Adding protocols ..."
   for s in File.session_choices:
     # different expressions
     for e in File.expression_choices[1:]:
+      if verbose > 1: print "  Adding expression '%s' protocol for session '%s'" % (e,s)
       session.add(Protocol('all', s, expression=e))
       session.add(Protocol('expression', s, expression=e))
 
     # different illuminations
     for i in File.illumination_choices[1:]:
+      if verbose > 1: print "  Adding illumination '%s' protocol for session '%s'" % (i,s)
       session.add(Protocol('all', s, illumination=i))
       session.add(Protocol('illumination', s, illumination=i))
 
     for o in File.occlusion_choices[1:]:
+      if verbose > 1: print "  Adding occlusion '%s' protocol for session '%s'" % (o,s)
       session.add(Protocol('all', s, occlusion=o))
       session.add(Protocol('occlusion', s, occlusion=o))
 
       # add mixed occlusion/illumination protocol
       for i in File.illumination_choices[1:3]:
+        if verbose > 1: print "  Adding occlusion '%s' and illumination '%s' protocol for session '%s'" % (o, i,s)
         session.add(Protocol('all', s, illumination=i, occlusion=o))
         session.add(Protocol('occlusion_and_illumination', s, illumination=i, occlusion=o))
 
     # add the neutral files to all of the protocols
     for protocol in Protocol.protocol_choices:
+      if verbose > 1: print "  Adding neutral files for session '%s'" % s
       session.add(Protocol(protocol, s, expression=File.expression_choices[0], illumination=File.illumination_choices[0], occlusion=File.occlusion_choices[0]))
 
 
@@ -125,9 +139,9 @@ def create(args):
   # the real work...
   create_tables(args)
   s = session_try_nolock(args.type, args.files[0], echo=(args.verbose >= 2))
-  add_clients(s)
-  add_files(s, args.directory, args.extension)
-  add_protocols(s)
+  add_clients(s, args.verbose)
+  add_files(s, args.directory, args.extension, args.verbose)
+  add_protocols(s, args.verbose)
   s.commit()
   s.close()
 
@@ -136,14 +150,9 @@ def add_command(subparsers):
 
   parser = subparsers.add_parser('create', help=create.__doc__)
 
-  parser.add_argument('-R', '--recreate', action='store_true', default=False,
-      help="If set, I'll first erase the current database")
-  parser.add_argument('-v', '--verbose', action='count',
-      help="Do SQL operations in a verbose way")
-  parser.add_argument('-D', '--directory', metavar='DIR',
-      default="/idiap/resource/database/AR_Face/images",
-      help="The path to the images of the AR face database")
-  parser.add_argument('--extension', metavar='STR', default='.ppm',
-      help="The file extension of the image files from the AR face database")
+  parser.add_argument('-R', '--recreate', action='store_true', help='If set, I\'ll first erase the current database')
+  parser.add_argument('-v', '--verbose', action='count', help='Do SQL operations in a verbose way?')
+  parser.add_argument('-D', '--directory', metavar='DIR', default='/idiap/resource/database/AR_Face/images', help='The path to the images of the AR face database')
+  parser.add_argument('--extension', metavar='STR', default='.ppm', help='The file extension of the image files from the AR face database')
 
   parser.set_defaults(func=create) #action
