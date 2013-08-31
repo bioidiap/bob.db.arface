ce01a5665687c832aca544aae0738814d915c69c
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-08-31 14:29:20 +0200

    Few fixes (copyright, keywords, etc) before python 3 compatible release

--- xbob/db/arface/create.py
+++ xbob/db/arface/create.py
@@ -3,7 +3,7 @@
 # @author: Manuel Guenther <Manuel.Guenther@idiap.ch>
 # @date:   Wed Jul  4 14:12:51 CEST 2012
 #
-# Copyright (C) 2011-2012 Idiap Research Institute, Martigny, Switzerland
+# Copyright (C) 2011-2013 Idiap Research Institute, Martigny, Switzerland
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
