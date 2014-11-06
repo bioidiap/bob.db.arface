94647c3e13756ad59612e4c7db484b32286e6db9
Manuel Guenther <manuel.guenther@idiap.ch>
2014-11-06 15:36:06 +0100

    Implemented versioning by version.txt

--- setup.py
+++ setup.py
@@ -19,12 +19,15 @@
 
 from setuptools import setup, find_packages
 
+# Define package version
+version = open("version.txt").read().rstrip()
+
 # The only thing we do in this file is to call the setup() function with all
 # parameters that define our package.
 setup(
 
     name='bob.db.arface',
-    version='2.0.0a0',
+    version=version,
     description='AR Face  Database Access API for Bob',
     url='https://pypi.python.org/pypi/bob.db.arface',
     license='GPLv3',
